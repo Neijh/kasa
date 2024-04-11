180512a552ac47a5108e38b2fa2f038bece5217d
gaedique <gaedique4@gmail.com>
2024-04-11 09:47:56 +0200

    Creation de la page erreur

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -2,7 +2,7 @@ import logo from '../../assets/error-logo.png'
 
 function Error() {
     return (
-        <div>
+        <div className='error'>
             <img src={ logo } alt="Erreur 404" />
             <h2>Oups! La page que vous demandez n'existe pas</h2>
             <p>Retourner sur la page d'accueil</p>
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-// import ReactDOM from 'react-dom'
 import { createRoot } from 'react-dom/client'
 import AppRouter from './components/AppRouter'
 import './styles/index.scss'
